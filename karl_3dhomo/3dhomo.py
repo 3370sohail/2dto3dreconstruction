17518708108891be623bf0139e9a78ecf5b02cd4
Karl Cui <karl.cui@mail.utoronto.ca>
2020-08-02 15:59:39 -0700

    code cleanup

--- karl_3dhomo/3dhomo.py
+++ karl_3dhomo/3dhomo.py
@@ -6,7 +6,8 @@ import numpy as np
 import open3d as o3d
 from scipy import optimize
 from skimage import io
-from sklearn.neighbors import NearestNeighbors
+
+from icp import icp
 
 
 def generate_keypoints_and_match(img1, img2):
@@ -26,7 +27,7 @@ def generate_keypoints_and_match(img1, img2):
     kp1, des1 = sift.detectAndCompute(img1, mask=None)
     kp2, des2 = sift.detectAndCompute(img2, mask=None)
 
-    matcher = cv2.BFMatcher(normType=cv2.NORM_L2)
+    matcher = cv2.BFMatcher(normType=cv2.NORM_L2, crossCheck=True)
     matches = matcher.match(des1, des2)
 
     return kp1, kp2, matches
@@ -34,24 +35,21 @@ def generate_keypoints_and_match(img1, img2):
 
 def refine_matches(matches):
     """
+    TODO: some other refinement method. Right now we are just taking the first couple of matches
 
     :param matches: list of matches
     :return: refined list of matches
     """
     matches = sorted(matches, key=lambda x: x.distance)
-
-    # good_matches = []
-    # for match in matches:
-    matches = matches[:7]
-
+    matches = matches[:20]
     return matches
 
 
 def pts_to_arr(pts, shape):
     """
-    Write points to csv file
+    Write points to image with shape shape
 
-    :param path: path to csv file to save to
+    :param pts: n x 3 ndarray, where the 3 is x y z coordinates
     :param shape: shape of image to write to
     :return: None
     """
@@ -199,139 +197,6 @@ def make_pcd(point_cloud):
     return pcd
 
 
-#
-#   From https://github.com/ClayFlannigan/icp/blob/master/icp.py
-#
-
-
-def best_fit_transform(A, B):
-    """
-    Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
-    Input:
-      A: Nxm numpy array of corresponding points
-      B: Nxm numpy array of corresponding points
-    Returns:
-      T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
-      R: mxm rotation matrix
-      t: mx1 translation vector
-    """
-    # get number of dimensions
-    m = A.shape[1]
-
-    # translate points to their centroids
-    centroid_A = np.mean(A, axis=0)
-    centroid_B = np.mean(B, axis=0)
-    AA = A - centroid_A
-    BB = B - centroid_B
-
-    # rotation matrix
-    H = np.dot(AA.T, BB)
-    U, S, Vt = np.linalg.svd(H)
-    R = np.dot(Vt.T, U.T)
-
-    # special reflection case
-    if np.linalg.det(R) < 0:
-       Vt[m-1, :] *= -1
-       R = np.dot(Vt.T, U.T)
-
-    # translation
-    t = centroid_B.T - np.dot(R, centroid_A.T)
-
-    # homogeneous transformation
-    T = np.identity(m+1)
-    T[:m, :m] = R
-    T[:m, m] = t
-
-    return T, R, t
-
-
-def nearest_neighbor(src, dst):
-    """
-    Find the nearest (Euclidean) neighbor in dst for each point in src
-    Input:
-        src: Nxm array of points
-        dst: Nxm array of points
-    Output:
-        distances: Euclidean distances of the nearest neighbor
-        indices: dst indices of the nearest neighbor
-    """
-    neigh = NearestNeighbors(n_neighbors=1)
-    neigh.fit(dst)
-    distances, indices = neigh.kneighbors(src, return_distance=True)
-    return distances.ravel(), indices.ravel()
-
-
-def icp(A, B, init_pose=None, max_iterations=20, tolerance=0.001):
-    """
-    The Iterative Closest Point method: finds best-fit transform that maps points A on to points B
-    Input:
-        A: Nxm numpy array of source mD points
-        B: Nxm numpy array of destination mD point
-        init_pose: (m+1)x(m+1) homogeneous transformation
-        max_iterations: exit algorithm after max_iterations
-        tolerance: convergence criteria
-    Output:
-        T: final homogeneous transformation that maps A on to B
-        distances: Euclidean distances (errors) of the nearest neighbor
-        i: number of iterations to converge
-    """
-    # get number of dimensions
-    m = A.shape[1]
-
-    # make points homogeneous, copy them to maintain the originals
-    src = np.ones((m+1, A.shape[0]))
-    dst = np.ones((m+1, B.shape[0]))
-    src[:m, :] = np.copy(A.T)
-    dst[:m, :] = np.copy(B.T)
-
-    # apply the initial pose estimation
-    if init_pose is not None:
-        src = np.dot(init_pose, src)
-
-    prev_error = 0
-
-    nntime = 0
-
-    for i in range(max_iterations):
-
-        nnstart = time.time()
-        # find the nearest neighbors between the current source and destination points
-        distances, indices = nearest_neighbor(src[:m, :].T, dst[:m, :].T)
-        nntime += time.time() - nnstart
-
-        # only take top
-        cutoff = int(src.shape[1] * 0.7)
-        smallest_idx = np.argpartition(distances, cutoff)[:cutoff]
-
-        distances = distances[smallest_idx]
-        indices = indices[smallest_idx]
-        src_smallest = src[:m, smallest_idx]
-
-        # compute the transformation between the current source and nearest destination points
-        T, _, _ = best_fit_transform(src_smallest[:m, :].T, dst[:m, indices].T)
-
-        # update the current source
-        src = np.dot(T, src)
-
-        # check error
-        mean_error = np.mean(distances)
-        if np.abs(prev_error - mean_error) < tolerance:
-            break
-        prev_error = mean_error
-
-    print("time spent calculating nearest neighbours: " + str(nntime))
-
-    # calculate final transformation
-    T, _, _ = best_fit_transform(A, src[:m, :].T)
-
-    return T, distances, i
-
-
-#
-#   Back to my own code
-#
-
-
 def apply_points_transformation(pts, transformation):
     """
     Apply a 3D homography transformation to a set of points
@@ -348,41 +213,38 @@ def apply_points_transformation(pts, transformation):
     return pts[:, :3] / np.expand_dims(pts[:, 3], axis=1)
 
 
-if __name__ == "__main__":
-    # load images
-    # img1 = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\car_pt_cloud\car1.jpg"
-    # img2 = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\car_pt_cloud\car2.jpg"
+def register_imgs(img1_rgb, img2_rgb, img1_depth, img2_depth, scale=1., filter_pts_frac=1., partial_set_frac=1.):
+    """
+    Perform global image registration given the RGB and depth of two images by
 
-    # # load point clouds
-    # img1_pts = np.genfromtxt(
-    #     r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\car_pt_cloud\car_0.csv", delimiter=",")
-    # img2_pts = np.genfromtxt(
-    #     r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\car_pt_cloud\car_1.csv", delimiter=",")
+        1. Performing global registration by extracting keypoints from RGB images
+        2.
 
-    # img1_depth = pts_to_arr(img1_pts, (img1.shape[0] // 2, img1.shape[1] // 2))
-    # img2_depth = pts_to_arr(img2_pts, (img2.shape[0] // 2, img2.shape[1] // 2))
-    # img1_depth = img1_depth.repeat(2, axis=0).repeat(2, axis=1)
-    # img2_depth = img2_depth.repeat(2, axis=0).repeat(2, axis=1)
+    :param img1_rgb: h x w x 3 image
+    :param img2_rgb: h x w x 3 image
+    :param img1_depth: h x w depth image
+    :param img2_depth: h x w depth image
+    :param scale: scaling factor for loading point clouds (see in-depth explanation in depth_to_voxel() function)
+    :param filter_pts_frac: fraction of all the points to use when performing ICP. Must be in range (0, 1], though
+                            choosing a good value would depend on the density of the depth we are working with.
+    :param partial_set_frac: fraction of expected overlap between the two images when performing ICP. Must be in
+                             range (0, 1]. In the case of video data, this value should usually be close to 1,
+                             though maybe not exactly 1, since we can expect a high overlap between frames of a
+                             video. Alternatively, if we are taking one frame from every couple of frames, this
+                             value should be slightly lower.
+    :return: image 1 point cloud,
+             image 2 point cloud,
+             4x4 transformation matrix that maps image 1 to image 2
+    """
+    # convert depth to point cloud
+    img1_pts = depth_to_voxel(img1_depth, scale=scale)
+    img2_pts = depth_to_voxel(img2_depth, scale=scale)
 
-    img1 = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_70.png"
-    img2 = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_72.png"
-    img1 = cv2.imread(img1)
-    img2 = cv2.imread(img2)
-
-    img1_depth = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_70_depth.png"
-    img2_depth = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_72_depth.png"
-    img1_depth = io.imread(img1_depth)
-    img2_depth = io.imread(img2_depth)
-
-    img1_pts = depth_to_voxel(img1_depth, scale=.7)
-    img2_pts = depth_to_voxel(img2_depth, scale=.7)
-    voxel_to_csv(img1_pts, r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\img70.csv")
-    voxel_to_csv(img2_pts, r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\img72.csv")
-
-    # find matches
-    kp1, kp2, matches = generate_keypoints_and_match(img1, img2)
+    # find RGB matches
+    kp1, kp2, matches = generate_keypoints_and_match(img1_rgb, img2_rgb)
     matches = refine_matches(matches)
 
+    # # draw matches
     # img = cv2.drawMatches(img1, kp1, img2, kp2, matches, outImg=None,
     #                       flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
     # cv2.imshow("matches", img)
@@ -397,22 +259,40 @@ if __name__ == "__main__":
 
     # find transformation
     h = homo_rigid_transform_3d(img1_kp, img2_kp)
-
-    # apply transformation
     img2_new = get_transformed_points(img1_pts, h)
 
-    np.savetxt(r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\img70_before_icp.csv",
-               img2_new, delimiter=",")
+    # filter points (since we have a dense point cloud)
+    pts_idx1 = np.random.choice(img2_new.shape[0], int(img2_new.shape[0] * filter_pts_frac), replace=False)
+    pts_idx2 = np.random.choice(img2_pts.shape[0], int(img2_pts.shape[0] * filter_pts_frac), replace=False)
 
-    start_time = time.time()
-    pts_idx1 = np.random.choice(img2_new.shape[0], img2_new.shape[0] // 30, replace=False)
-    pts_idx2 = np.random.choice(img2_pts.shape[0], img2_pts.shape[0] // 30, replace=False)
+    # ICP for fine registration
+    t, _, iter_to_converge = icp(img2_new[pts_idx1], img2_pts[pts_idx2],
+                                 tolerance=0.001,
+                                 max_iterations=100,
+                                 partial_set_size=partial_set_frac)
 
-    t, _, iter_to_converge = icp(img2_new[pts_idx1], img2_pts[pts_idx2], tolerance=0.001, max_iterations=100)
-    print("iterations to converge: " + str(iter_to_converge))
-    print("time taken: " + str(time.time() - start_time))
+    # return point clouds and transformation
+    return img1_pts, img2_pts, np.dot(h, t)
 
-    img2_new = apply_points_transformation(img2_new, t)
+
+if __name__ == "__main__":
+    # load RGB images
+    img1 = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_70.png"
+    img2 = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_72.png"
+    img1 = cv2.imread(img1)
+    img2 = cv2.imread(img2)
+
+    # load RGB point clouds
+    img1_depth = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_70_depth.png"
+    img2_depth = r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\kitchen_small_1_72_depth.png"
+    img1_depth = io.imread(img1_depth)
+    img2_depth = io.imread(img2_depth)
+
+    # find transformation from first image to second image
+    img1_pts, img2_pts, homo = register_imgs(img1, img2, img1_depth, img2_depth, scale=.7)
+
+    # apply transformation
+    img2_new = apply_points_transformation(img1_pts, homo)
 
     # save
     np.savetxt(r"C:\Users\karlc\Documents\ut\_y4\CSC420\project\3d_reconstruction\imgs\kitchen\img70_after_icp.csv",
---
+++ karl_3dhomo/icp.py
@@ -0,0 +1,124 @@
+"""
+Most of this code from https://github.com/ClayFlannigan/icp/blob/master/icp.py
+
+Modified by us to
+    1. Not match 100% of points (ie. partial match)
+    2. Modified the operations in the code to run faster. Since the code from the GitHub was mostly vectorized
+       already, the main difference here is we know how many dimensions we are dealing with at all times since
+       we are doing 3D ICP and not n-dimensional ICP, so we can avoid a lot of array slicing in the original code.
+"""
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+
+def best_fit_transform(A, B):
+    """
+    Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
+    Input:
+      A: 3xm numpy array of corresponding points
+      B: 3xm numpy array of corresponding points
+    Returns:
+      T: 4x4 homogeneous transformation matrix that maps A on to B
+    """
+    # translate points to their centroids
+    centroid_A = np.mean(A, axis=0)
+    centroid_B = np.mean(B, axis=0)
+    AA = A - centroid_A
+    BB = B - centroid_B
+
+    # rotation matrix
+    H = np.dot(AA.T, BB)
+    U, S, Vt = np.linalg.svd(H)
+    R = np.dot(Vt.T, U.T)
+
+    # special reflection case
+    if np.linalg.det(R) < 0:
+       Vt[2, :] *= -1
+       R = np.dot(Vt.T, U.T)
+
+    # translation
+    t = centroid_B.T - np.dot(R, centroid_A.T)
+
+    # homogeneous transformation
+    T = np.eye(4)
+    T[:3, :3] = R
+    T[:3, 3] = t
+
+    return T
+
+
+def nearest_neighbor(src, dst):
+    """
+    Find the nearest (Euclidean) neighbor in dst for each point in src
+    Input:
+        src: 3xm array of points
+        dst: 3xm array of points
+    Output:
+        distances: Euclidean distances of the nearest neighbor
+        indices: dst indices of the nearest neighbor
+    """
+    neigh = NearestNeighbors(n_neighbors=1)
+    neigh.fit(dst)
+    distances, indices = neigh.kneighbors(src, return_distance=True)
+    return distances.ravel(), indices.ravel()
+
+
+def icp(A, B, init_pose=None, max_iterations=20, tolerance=0.001, partial_set_size=1.):
+    """
+    The Iterative Closest Point method: finds best-fit transform that maps points A on to points B
+
+    Input:
+        A: 3xm numpy array of source mD points
+        B: 3xm numpy array of destination mD point
+        init_post: 4x4 homogenous transformation
+        max_iterations: exit algorithm after max_iterations
+        tolerance: convergence criteria
+        partial_set_size: float in range (0, 1] which describes what portion of the point clouds have to
+                          match in ICP
+
+    Output:
+        T: final homogeneous transformation that maps A on to B
+        distances: Euclidean distances (errors) of the nearest neighbor
+        i: number of iterations to converge
+    """
+    # make points homogeneous, copy them to maintain the originals
+    src = np.concatenate((A.T, np.ones((1, A.shape[0]))), axis=0)
+    dst = np.concatenate((B.T, np.ones((1, B.shape[0]))), axis=0)
+
+    # apply the initial pose estimation
+    if init_pose is not None:
+        src = np.dot(init_pose, src)
+
+    prev_error = 0
+    distances = None
+    iters = 1
+    while iters <= max_iterations:
+        # find the nearest neighbors between the current source and destination points
+        distances, indices = nearest_neighbor(src[:3, :].T, dst[:3, :].T)
+
+        # partial match: only take top fraction of correspondences for matching after finding nearest neighbour
+        cutoff = int(src.shape[1] * partial_set_size)
+        smallest_idx = np.argpartition(distances, cutoff - 1)[:cutoff]
+
+        distances = distances[smallest_idx]
+        indices = indices[smallest_idx]
+        src_smallest = src[:3, smallest_idx]
+
+        # compute the transformation between the current source and nearest destination points
+        t = best_fit_transform(src_smallest[:3, :].T, dst[:3, indices].T)
+
+        # update the current source
+        src = np.dot(t, src)
+
+        # check error
+        mean_error = np.mean(distances)
+        if np.abs(prev_error - mean_error) < tolerance:
+            break
+        prev_error = mean_error
+
+        iters += 1
+
+    # calculate final transformation
+    t = best_fit_transform(A, src[:3, :].T)
+
+    return t, distances, iters
